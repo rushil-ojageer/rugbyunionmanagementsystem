fe502e83aa392b0c37b5c3a8eee8265eab23e867
Rushil <rushilojageer@gmail.com>
2020-11-17 07:59:43 +0200

    Changed error handling

---
+++ RugbyUnion.ManagementSystem/RugbyUnion.ManagementSystem/Controllers/ErrorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RugbyUnion.ManagementSystem.Models;
+using System.Net;
+
+namespace RugbyUnion.ManagementSystem.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorsController : ControllerBase
+    {
+        [Route("error")]
+        public ErrorResponse Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = context?.Error; 
+            Response.StatusCode = (int) HttpStatusCode.BadRequest;
+            return new ErrorResponse(exception);
+        }
+    }
+}
---
+++ RugbyUnion.ManagementSystem/RugbyUnion.ManagementSystem/Models/ErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RugbyUnion.ManagementSystem.Models
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; }
+
+        public ErrorResponse(Exception ex)
+        {
+            Message = ex.Message;
+        }
+    }
+}
