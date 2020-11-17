519ba90bb43bec8b433d6aabcacf35044a70cecf
Rushil <rushilojageer@gmail.com>
2020-11-17 08:01:23 +0200

    Handled ModelState validation errors

---
+++ RugbyUnion.ManagementSystem/RugbyUnion.ManagementSystem/Filters/ValidateModelActionFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RugbyUnion.ManagementSystem.Filters
+{
+    public class ValidateModelActionFilter : IFilterMetadata
+    {
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new ObjectResult(context.ModelState)
+                {
+                    StatusCode = StatusCodes.Status400BadRequest,
+                };
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+
+        }
+    }
+}
--- RugbyUnion.ManagementSystem/RugbyUnion.ManagementSystem/Startup.cs
+++ RugbyUnion.ManagementSystem/RugbyUnion.ManagementSystem/Startup.cs
@@ -7,10 +7,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RugbyUnion.ManagementSystem.Data;
+using RugbyUnion.ManagementSystem.Filters;
 using RugbyUnion.ManagementSystem.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 
 namespace RugbyUnion.ManagementSystem
@@ -28,8 +30,16 @@ namespace RugbyUnion.ManagementSystem
         public void ConfigureServices(IServiceCollection services)
         {
             // Controllers
-            services.AddControllers();
-
+            services.AddControllers(options => options.Filters.Add<ValidateModelActionFilter>())
+                            .ConfigureApiBehaviorOptions(options =>
+                            {
+                                options.InvalidModelStateResponseFactory = context =>
+                                {
+                                    var result = new BadRequestObjectResult(context.ModelState);
+                                    result.ContentTypes.Add(MediaTypeNames.Application.Json);
+                                    return result;
+                                };
+                            });
             // Database
             services.AddDbContext<RugbyUnionContext>(opt => opt.UseInMemoryDatabase("RubgyUnionManagementDatabase").UseLazyLoadingProxies());
 
